763d956bea3de20a0bb9624a5eaa32d9d8c1b2d3
idlebrain87 <99801982+idlebrain87@users.noreply.github.com>
2022-02-19 13:18:02 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# Create  a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+my_dict = {}
+
+for i in range(97, 97 + 26):
+
+    my_dict[chr(i)] = i
+
+
+print(my_dict)
